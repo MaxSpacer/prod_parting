ba56a5a7c3d6baeb35c3b827c4872102bbb80896
MaxSpacer <max_spacer@mail.ru>
2026-05-13 19:58:36 +0300

    k

--- parties/signals.py
+++ parties/signals.py
@@ -15,62 +15,16 @@ from django.conf import settings
 import logging
 from parties.scanner import barcode_reader
 import csv
+import time
+# import asyncio
+from crontab import CronTab
 
 @receiver(post_save, sender=Party)
 def get_scan(sender, instance, created, **kwargs):
     if instance.status:
         print(...)
+        my_cron = CronTab(user='max_spacer')
 
-        file_out = os.path.join(
-            settings.MEDIA_ROOT, instance.report_party_file)
-        
-        with open(file_out, "w"):
-            pass
-        
-        if file_out:
-            
-            with open(file_out, mode='w', newline='') as file_out:
-                row_writer = csv.writer(file_out, delimiter=';')
-                
-                try:
-                    while True:
-                        scanned_code = barcode_reader()
-                        if scanned_code:
-                            # file_out.write(scanned_code + "\n")
-                            # file_out.flush()
-                            row_writer.writerow([scanned_code,])
-                            print(f"Сохранено: {scanned_code}")
-                            # print('test -', upcnumber)
-                except KeyboardInterrupt:
-                    logging.debug('Keyboard interrupt')
-                except Exception as err:
-                    logging.error(err)
-            
-        
-    # if created:
-    #     context = {
-	# 	    'contact_name': instance.customer_name,
-	# 	    'callme_number': instance.id,
-	# 	    'contact_phone': instance.customer_phone,
-	# 	}
-    #     subject = 'Заказ звонка № call-%s' % instance.id
-    #     html_message = render_to_string('mail_templates/mail_callme_template.html', context)
-    #     plain_message = strip_tags(html_message)
-    #     from_email = 'info@dohaich.ru'
-    #     to = 'zakaz@dohaich.ru'
-    #     # print(instance.is_emailed)
-    #     if instance.is_emailed == False:
-    #         if subject and html_message and from_email:
-    #             try:
-    #                 if send_mail(subject, plain_message, from_email, [to], html_message=html_message):
-    #                     Callmecontact.objects.filter(pk=instance.pk).update(is_emailed=True)
-    #                     instance.is_emailed = True
-    #                     # print(instance.is_emailed)
-
-    #             except BadHeaderError:
-    #                 print('Invalid header found in email %s' % instance.id)
-    #             # return HttpResponse('Invalid header found %s' % instance.id)
-    #             # education_order.is_emailed = True
-    #             return HttpResponse('sended')
-    #         else:
-    # 	        return print('Make sure all fields are entered and valid %s' % instance.id)
+        job = my_cron.new(command=f'/home/max_spacer/prod_parting/.venv/bin/python /home/max_spacer/prod_parting/manage.py scan_job_command 0 {instance.report_party_file}', comment='scan_job')
+        for jo in my_cron:
+            print(jo)    
--- prod_parting/management/commands/scan_job_command.py
+++ prod_parting/management/commands/scan_job_command.py
@@ -4,29 +4,46 @@ from parties.scanner import barcode_reader
 import os
 import re
 import csv
-# import libusb_package
-import usb
-# from django.conf import settings
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
-
+import logging
 
 class Command(BaseCommand):
-    help = "(Re)parse it."
+    help = "Scan_job_command."
 
     def add_arguments(self, parser):
         parser.add_argument('bool', nargs='?', default=False)
+        parser.add_argument('filebrowserfilename', nargs='?', default='')
 
     def handle(self, *args, **options):
         bool = options['bool']
-
+        filebrowser_file_name = options['filebrowser_file_name']
+        
         if not bool:
             raise CommandError('<bool> must be a bool.')
         else:
             self.stdout.write('START\n')
 
-            import logging
+            
+            file_out = os.path.join(
+                settings.MEDIA_ROOT, filebrowser_file_name)
 
-            # if __name__ == '__main__':
+            with open(file_out, mode='w', newline='') as file_out:
+                try:
+                    while True:
+                        print("tsert: ", filebrowser_file_name)
+                        # scanned_code = barcode_reader()
+                        # if scanned_code:
+                            # file_out.write(scanned_code + "\n")
+                            # file_out.flush()
+                            # print(f"Сохранено: {scanned_code}")
+                        break
+                except KeyboardInterrupt:
+                    logging.debug('Keyboard interrupt')
+                except Exception as err:
+                    logging.error(err)
+
+            
         
             try:
                 while True:
