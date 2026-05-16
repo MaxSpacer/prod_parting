27936438ed60edbfee9463c059686adabb614a22
MaxSpacer <max_spacer@mail.ru>
2026-05-17 01:29:31 +0300

    g

--- parties/admin.py
+++ parties/admin.py
@@ -8,6 +8,8 @@ admin.site.site_header = "Контроль партий";
 admin.site.site_title = "Контроль партий";
 
 class PartyAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Party._meta.fields]
+    list_display = ["platform_number",
+                    "status",
+                    "report_party_file"]
 admin.site.register(Party, PartyAdmin)
 
