64cf02c983def3b5e075f67c129c0ef34cc8400f
MaxSpacer <max_spacer@mail.ru>
2026-05-17 03:30:50 +0300

    D

--- parties/daemon.py
+++ parties/daemon.py
@@ -220,9 +220,9 @@ class testdaemon(Daemon):
 
 daemon = testdaemon()
 
-if 'start' == sys.argv[1] and sys.argv[2]:
-    daemon.start(path = sys.argv[2])
-elif 'start' == sys.argv[1] :
+# if 'start' == sys.argv[1] and sys.argv[2]:
+#     daemon.start(path = sys.argv[2])
+if 'start' == sys.argv[1] :
     daemon.start()
 elif 'stop' == sys.argv[1]:
     daemon.stop()
