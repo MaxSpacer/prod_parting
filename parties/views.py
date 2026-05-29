febcbb40d48fec564ec309893cf9051089956d59
MaxSpacer <max_spacer@mail.ru>
2026-05-29 17:45:57 +0300

    up

--- parties/views.py
+++ parties/views.py
@@ -16,7 +16,7 @@ def create_xls(request, item):
     if os.path.isfile(file_path_xls):
         os.remove(file_path_xls)
 
-    with open(party.report_party_file.path_full, mode="r", encoding="utf-8") as csv_file, \
+    with open(party.report_party_file.path_full, mode="r", encoding="utf-8", newline='') as csv_file, \
         pd.ExcelWriter(file_path_xls, engine="xlsxwriter") as writer:  
         csv_reader = csv.reader(csv_file)
         
